db78c95f93181f8bb90ab016cf15d2dd4e9ab77f
William Bugenis <wbugenis@gmail.com>
2021-02-25 17:37:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const inputInput = document.querySelector("input#input")
+    inputInput.addEventListener('click', () => {
+        console.log(event)
+        alert(event)    
+    })
 }
+
+addingEventListener()
